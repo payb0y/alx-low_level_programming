ca7a45cfc507da09201b5d7ea9a58afa382a93d1
Othmane Gattouchi <othmane.Gat@gmail.com>
2023-03-17 14:36:01 -0700

    Solution

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int n, m;
 
-	for(n = 48; n <= 56; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for(m = 49; m <= 57; m++)
+		for (m = 49; m <= 57; m++)
 		{
-			if(m > n)
+			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
